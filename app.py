1b7b843f65d62b6ae3694f89852ced5437385cdb
Alfredo Moura <alfred.mou99@gmail.com>
2024-03-22 23:12:55 -0300

    finalizando projeto

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 
-restaurantes = ['Parmegianno', 'Açai', 'Unimed']
+restaurantes = [{'nome': 'Praça', 'categoria': 'Japonesa' , 'ativo':False},
+                {'nome': 'Pizza Suprema', 'categoria': 'Pizza' , 'ativo':True},
+                {'nome': 'Cantina', 'categoria': 'Italiana' , 'ativo':False}]
 
 def exibir_nome_do_programa():
       print("""Sabor Express
@@ -9,11 +11,11 @@ def exibir_nome_do_programa():
 def exibir_opcoes():
       print('1. Cadastrar restaurante')
       print('2. Listar restaurante')
-      print('3. Ativar restaurante')
+      print('3. Alternar status do restaurante')
       print('4. Sair\n')
 
 def finalizar_app():
-      exibir_subtituto('Finalizar app')
+      exibir_subtitulo('Finalizar app')
 
       
 def voltar_ao_menu_principal():
@@ -24,22 +26,48 @@ def opcao_invalida():
       print('Opção inválida!\n')
       voltar_ao_menu_principal()
 
-def exibir_subtituto(texto):
+def exibir_subtitulo(texto):
       os.system('cls')
+      linha = '*' * (len(texto))
+      print(linha)
       print(texto)
+      print(linha)
       print()
 
 def cadastrar_novo_restaurante():
-      exibir_subtituto('Cadastrar restaurantes.')
-      nome_do_restaurante = input('Digite o nome do seu restaurante: \n')
-      restaurantes.append(nome_do_restaurante)
+      exibir_subtitulo('Cadastrar restaurantes.')
+      nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: \n')
+      categoria = input(f'Digite a categoria do restaurante {nome_do_restaurante}: ')
+      dados_do_restaurante = {'nome': nome_do_restaurante, 'categoria': categoria, 'ativo': False}
+      restaurantes.append(dados_do_restaurante)
       print(f'O restaurante {nome_do_restaurante} foi cadastrado com sucesso!\n')
       voltar_ao_menu_principal()
 
 def listar_restaurantes():
-      exibir_subtituto('Listando os restaurantes\n')
+      exibir_subtitulo('Listando os restaurantes\n')
+
+      print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria'.ljust(20)} | Status')
       for restaurante in restaurantes:
-            print(f'.{restaurante}')
+            nome_restaurante = restaurante['nome']
+            categoria = restaurante['categoria']
+            ativo = 'ativado' if restaurante['ativo'] else 'desativado'
+            print(f'- {nome_restaurante.ljust(20)} | {categoria.ljust(20)} | {ativo}')
+      voltar_ao_menu_principal()
+
+def alternar_estado_restaurante():
+      exibir_subtitulo('Alterando estado do restaurante')
+      nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
+      restaurante_encontrado = False
+
+      for restaurante in restaurantes:
+            if nome_restaurante == restaurante['nome']:
+                  restaurante_encontrado = True
+                  restaurante['ativo'] = not restaurante['ativo']
+                  mensagem = f'O restaurante {nome_restaurante} foi ativado com sucesso' if restaurante['ativo'] else f'O restaurante {nome_restaurante} foi desativado com sucesso'
+                  print(mensagem)
+      if not restaurante_encontrado:
+            print('O restaurante não foi encontrado')
+      
       voltar_ao_menu_principal()
 
 def escolher_opcao():
@@ -51,6 +79,7 @@ def escolher_opcao():
             elif opcao_escolhida == 2:
                   listar_restaurantes() 
             elif opcao_escolhida == 3:
+                  alternar_estado_restaurante()
                   print('Ativar restaurante')
             elif opcao_escolhida == 4:
                   finalizar_app()
--- exercicio.py
+++ exercicio.py
@@ -1,14 +1,40 @@
-notas = [2,4,6,8,10]
-tamanho = len(notas)
-soma = 0
+# # Palavra de entrada
+# palavra = "banana"
 
-try:
-    for nota in notas:
-        soma += nota
-        print(nota)
-    media = soma / tamanho
-    print(media)
-except ZeroDivisionError:
-    print(f"A lista está vazia")
-except Exception as e:
-    print(f"A lista possui um erro: {e}")
+# # Inicializa o dicionário de contagem de letras
+# contagem_letras = {}
+
+# # Itera sobre cada letra na palavra
+# for letra in palavra:
+#     # Verifica se a letra já está no dicionário
+#     if letra in contagem_letras:
+#         # Incrementa a contagem se a letra já estiver no dicionário
+#         contagem_letras[letra] += 1
+#     else:
+#         # Inicializa a contagem como 1 se a letra não estiver no dicionário
+#         contagem_letras[letra] = 1
+
+# # Imprime o dicionário de contagem de letras
+# print(contagem_letras)
+
+
+# frase = "Python se tornou uma das linguagens de programação mais populares do mundo nos últimos anos uma"
+# contagem_palavras = {}
+# palavras = frase.split()
+
+# for palavra in palavras:
+#     if palavra in contagem_palavras:
+#         contagem_palavras[palavra] +=1
+#     else: contagem_palavras[palavra] = 1
+
+
+
+# print(contagem_palavras)
+
+lista_de_numeros = [1,2,3,4,5,6,7, 'batata']
+
+
+for numero in lista_de_numeros:
+    print(numero)
+
+
